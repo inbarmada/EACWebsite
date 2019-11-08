ac745f48ffeeee69be102de28cf9cbf8ae9d6b6e
inbarmada <inbar.mada@gmail.com>
2019-11-07 20:40:03 -0600

    Create home page (as php file) and css

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <title>StudySpanish</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+    <div class="menu">
+        <ul>
+            <li style="float: left">AprenderEspa&ntilde;ol</li>
+            <li style="float: right">Log in</li>
+            <li style="float: right">Sign up</li>
+        </ul>
+    </div>
+    <div class="full-screen" id="screen-1">
+          <div class="title">APRENDER ESPA&Ntilde;OL</div>
+    </div>
+
+    <div class="full-screen" id="screen-2">
+      <div class="half-screen-image" style="float:left">
+        <img class="fill-image" src="index-screen-2.jpg" alt="Image of a book in Spanish"></img>
+      </div>
+      <div class="half-screen-text" style="float:right"> <div class="half-screen-text-inner">
+        <div class="half-screen-title">Spanish</div>
+        <div class="half-screen-content">
+          hohonohohono hohonohohono hohon ohohonohohono
+          hohonohohono hohonohohonohohon ohohonohohono hohonohohon ohohononohohonoho
+          honohoh onohohonohohonohohonohoho nohohononohohono hohonohohonohohonoh ohonohohonohohonohohono
+          nohohonoh ohonohohonoh ohonohoho nohohonohohon ohohononohoho nohohonoh
+          ohonohohonohoh onohohonohohonoho hononohohon ohohonoho honoho honohohonohoho
+          nohohonoho hononoho honoho honohoh onohohonohohon ohohonoh ohonohohono
+        </div>
+      </div></div>
+    </div>
+
+    <div class="full-screen" id="screen-3">
+      <div class="half-screen-text" style="float:left"> <div class="half-screen-text-inner">
+        <div class="half-screen-title">Spanish</div>
+        <div class="half-screen-content">
+          hohonohohono hohonohohono hohon ohohonohohono
+          hohonohohono hohonohohonohohon ohohonohohono hohonohohon ohohononohohonoho
+          honohoh onohohonohohonohohonohoho nohohononohohono hohonohohonohohonoh ohonohohonohohonohohono
+          nohohonoh ohonohohonoh ohonohoho nohohonohohon ohohononohoho nohohonoh
+          ohonohohonohoh onohohonohohonoho hononohohon ohohonoho honoho honohohonohoho
+          nohohonoho hononoho honoho honohoh onohohonohohon ohohonoh ohonohohono
+        </div>
+      </div></div>
+      <div class="half-screen-image" style="float:right">
+        <img class="fill-image" src="index-screen-3.jpg" alt="Image of Machu Picchu"></img>
+      </div>
+    </div>
+
+    <div class="full-screen" id="screen-4">
+          <div class="title"><a href="login.html">Begin Journey &rarr;</a></div>
+    </div>
+</body>
+</html>
